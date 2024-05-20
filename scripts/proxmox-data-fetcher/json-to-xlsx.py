b6f915bc81060312ed0ef74e905a427656263d3b
Hatam Abolghasemi <hatamabolghasemi@gmail.com>
2024-05-20 13:09:18 +0330

    fix json-to-xlsx.py

--- scripts/proxmox-data-fetcher/json-to-xlsx.py
+++ scripts/proxmox-data-fetcher/json-to-xlsx.py
@@ -11,8 +11,6 @@ bash_script = "proxmox-data-fetcher.sh"
 # initialize
 if os.path.exists(xlsx_directory):
     shutil.rmtree(xlsx_directory)
-
-if not os.path.exists(xlsx_directory):
     os.makedirs(xlsx_directory)
 
 # SKIP THIS STEP IF YOUR PROXMOX DATA IS UP-TO-DATE
@@ -29,6 +27,23 @@ print(f"Converting JSON to XLSX.")
 def json_to_dataframe(json_data):
     data_list = json_data.get("data", [])
     return pd.DataFrame(data_list)
+
+for json_filename in os.listdir(json_directory):
+    if json_filename.endswith('.json'):
+        json_filepath = os.path.join(json_directory, json_filename)
+        
+        try:
+            with open(json_filepath, 'r') as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            pass
+        except json.JSONDecodeError:
+            pass
+        
+        df = json_to_dataframe(data)
+        xlsx_filename = f"{os.path.splitext(json_filename)[0]}.xlsx"
+        xlsx_filepath = os.path.join(xlsx_directory, xlsx_filename)
+        df.to_excel(xlsx_filepath, index=False)
 
 for item in os.listdir(json_directory):
     item_path = os.path.join(json_directory, item)
