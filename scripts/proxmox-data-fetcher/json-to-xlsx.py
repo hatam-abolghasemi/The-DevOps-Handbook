e16a65293c92231755c21875d2c959a472ce4cd3
Hatam Abolghasemi <hatamabolghasemi@gmail.com>
2024-05-25 13:59:33 +0330

    update proxmox scripts

--- scripts/proxmox-data-fetcher/json-to-xlsx.py
+++ scripts/proxmox-data-fetcher/json-to-xlsx.py
@@ -8,11 +8,6 @@ xlsx_directory = "xlsx"
 json_directory = "json"
 bash_script = "proxmox-data-fetcher.sh"
 
-# initialize
-if os.path.exists(xlsx_directory):
-    shutil.rmtree(xlsx_directory)
-    os.makedirs(xlsx_directory)
-
 # SKIP THIS STEP IF YOUR PROXMOX DATA IS UP-TO-DATE
 # fetch proxmox data as json
 print(f"Fetching data as JSON.")
@@ -21,8 +16,13 @@ try:
 except subprocess.CalledProcessError as e:
     pass
 
+# initialize
+if os.path.exists(xlsx_directory):
+    shutil.rmtree(xlsx_directory)
+os.makedirs(xlsx_directory)
+
 # convert json to xlsx
-print(f"Converting JSON to XLSX.")
+print("Converting JSON to XLSX.")
 
 def json_to_dataframe(json_data):
     data_list = json_data.get("data", [])
@@ -35,42 +35,13 @@ for json_filename in os.listdir(json_directory):
         try:
             with open(json_filepath, 'r') as f:
                 data = json.load(f)
+            df = json_to_dataframe(data)
+            xlsx_filename = f"{os.path.splitext(json_filename)[0]}.xlsx"
+            xlsx_filepath = os.path.join(xlsx_directory, xlsx_filename)
+            df.to_excel(xlsx_filepath, index=False)
         except FileNotFoundError:
-            pass
+            print(f"File {json_filename} not found.")
         except json.JSONDecodeError:
-            pass
-        
-        df = json_to_dataframe(data)
-        xlsx_filename = f"{os.path.splitext(json_filename)[0]}.xlsx"
-        xlsx_filepath = os.path.join(xlsx_directory, xlsx_filename)
-        df.to_excel(xlsx_filepath, index=False)
-
-for item in os.listdir(json_directory):
-    item_path = os.path.join(json_directory, item)
-    
-    if os.path.isdir(item_path):
-        xlsx_subdir = os.path.join(xlsx_directory, item)
-        if not os.path.exists(xlsx_subdir):
-            os.makedirs(xlsx_subdir)
-        
-        for json_filename in os.listdir(item_path):
-            if json_filename.endswith('.json'):
-                json_filepath = os.path.join(item_path, json_filename)
-                
-                try:
-                    with open(json_filepath, 'r') as f:
-                        data = json.load(f)
-
-                    df = json_to_dataframe(data)
-                    
-                    xlsx_filename = f"{os.path.splitext(json_filename)[0]}.xlsx"
-                    xlsx_filepath = os.path.join(xlsx_subdir, xlsx_filename)
-                    
-                    df.to_excel(xlsx_filepath, index=False)
-                
-                except FileNotFoundError:
-                    pass
-                except json.JSONDecodeError:
-                    pass
-                except ValueError as e:
-                    pass
+            print(f"Error decoding JSON from file {json_filename}.")
+        except Exception as e:
+            print(f"An unexpected error occurred with file {json_filename}: {e}")
--- scripts/proxmox-data-fetcher/proxmox-data-fetcher.sh
+++ scripts/proxmox-data-fetcher/proxmox-data-fetcher.sh
@@ -2,9 +2,6 @@
 
 # initialize
 source .env
-if ! test -d json; then
-  mkdir json
-fi
 cd json
 
 # cluster status
@@ -19,146 +16,67 @@ echo -ne "Fetching cluster resources\r"
 curl -s -H "Authorization: PVEAPIToken=$API_TOKEN" "$CLUSTER_URL""cluster/resources" | jq . >> cluster-resources.json
 echo "Cluster resources is saved to cluster-resources.json"
 
-# data processing
-rm qemu-resources.json node-resources.json storage-resources.json
-jq '.data | map(select(.type == "qemu")) | {data: .}' cluster-resources.json >> qemu-resources.json
-jq '.data | map(select(.type == "node")) | {data: .}' cluster-resources.json >> node-resources.json
-jq '.data | map(select(.type == "storage")) | {data: .}' cluster-resources.json >> storage-resources.json
+# pre processing
+jq '.data | map(select(.type == "qemu")) | {data: .}' cluster-resources.json >> vm-summary.tmp
+jq '.data | map(select(.type == "node")) | {data: .}' cluster-resources.json >> node-summary.tmp
+jq '.data | map(select(.type == "storage")) | {data: .}' cluster-resources.json >> storage-summary.tmp
 
 jq -r '.data[] | select(.type == "node") .name' cluster-status.json >> node-names.tmp
-jq -r '.data[] | select(.type == "qemu") | "\(.node)/\(.id)"' cluster-resources.json >> node-resources.tmp
-while read -r node_name; do
-    mkdir -p "$node_name"
-done < "node-names.tmp"
+mv cluster-status.json cluster-status.tmp
 
-# rrd stats
-echo -ne "Fetching node RRD statistics\r"
-while IFS= read -r node_name; do
-    NODE_RRD_STATS_URL="$CLUSTER_URL""nodes/$node_name/rrddata?timeframe=hour&cf=AVERAGE"
-    if [ -f "$node_name/node-rrd-stats.json" ]; then
-        rm "$node_name/node-rrd-stats.json"
+# post processing
+echo -ne "Processing the results\r"
+grep -v -e "type" -e '"id"' storage-summary.tmp > storage-summary.tmp.2
+grep -v -e "type" -e '"id"' -e '"cpu"' -e '"mem"' -e '"disk"' -e "net" -e "diskwrite" -e "diskread" -e "template" -e "uptime" vm-summary.tmp > vm-summary.tmp.2
+grep -v -e "type" -e '"id"' -e '"cpu"' -e '"mem"' -e "level" -e "uptime" node-summary.tmp > node-summary.tmp.2
+grep -v -e "type" -e '"id"' -e "level" cluster-status.tmp > cluster-status.json
+
+convert_bytes_to_gb() {
+    local bytes=$1
+    echo "scale=2; $bytes / (1024 * 1024 * 1024)" | bc
+}
+
+while IFS= read -r line; do
+    if [[ "$line" =~ disk ]]; then
+        value=$(echo "$line" | grep -oP '(?<=: )[0-9]+')
+        gb_value=$(convert_bytes_to_gb "$value")
+        line=$(echo "$line" | sed -r "s/: [0-9]+/: $gb_value/")
     fi
-    curl -s -H "Authorization: PVEAPIToken=$API_TOKEN" "$NODE_RRD_STATS_URL" | jq . >> "$node_name/node-rrd-stats.json"
-done < "node-names.tmp"
-echo "Nodes RRD stats is saved to each node's directory as node-rrd-stats.json"
+    echo "$line"
+done < storage-summary.tmp.2 > storage-summary.json
 
-# node status
-echo -ne "Fetching node status\r"
-while IFS= read -r node_name; do
-    NODE_STATUS_URL="$CLUSTER_URL""nodes/$node_name/status"
-    if [ -f "$node_name/node-status.json" ]; then
-        rm "$node_name/node-status.json"
+while IFS= read -r line; do
+    if [[ "$line" =~ maxdisk ]] || [[ "$line" =~ maxmem ]]; then
+        value=$(echo "$line" | grep -oP '(?<=: )[0-9]+')
+        gb_value=$(convert_bytes_to_gb "$value")
+        line=$(echo "$line" | sed -r "s/: [0-9]+/: $gb_value/")
     fi
-    curl -s -H "Authorization: PVEAPIToken=$API_TOKEN" "$NODE_STATUS_URL" | jq . >> "$node_name/node-status.json"
-done < "node-names.tmp"
-echo "Nodes status is saved to each node's directory as node-status.json"
+    echo "$line"
+done < vm-summary.tmp.2 > vm-summary.json
 
-# node time
-echo -ne "Fetching node time\r"
-while IFS= read -r node_name; do
-    NODE_TIME_URL="$CLUSTER_URL""nodes/$node_name/time"
-    if [ -f "$node_name/node-time.json" ]; then
-        rm "$node_name/node-time.json"
+while IFS= read -r line; do
+    if [[ "$line" =~ disk ]] || [[ "$line" =~ maxmem ]]; then
+        value=$(echo "$line" | grep -oP '(?<=: )[0-9]+')
+        gb_value=$(convert_bytes_to_gb "$value")
+        line=$(echo "$line" | sed -r "s/: [0-9]+/: $gb_value/")
     fi
-    curl -s -H "Authorization: PVEAPIToken=$API_TOKEN" "$NODE_TIME_URL" | jq . >> "$node_name/node-time.json"
-done < "node-names.tmp"
-echo "Nodes time is saved to each node's directory as node-time.json"
+    echo "$line"
+done < node-summary.tmp.2 > node-summary.json
 
-# node qemu status
-echo -ne "Fetching node QEMU status\r"
-while IFS= read -r qemu_name; do
-    NODE_QEMU_STATUS_URL="$CLUSTER_URL""nodes/$qemu_name/status/current"
-    qemu_name_dashed=$(echo "$qemu_name" | sed 's/\//-/g')
-    node_name=${qemu_name_dashed%%-qemu*}
-    pure_qemu=${qemu_name_dashed##*qemu-}
-    if [ -f "$node_name/$pure_qemu-status.json" ]; then
-        rm "$node_name/$pure_qemu-status.json"
-    fi
-    curl -s -H "Authorization: PVEAPIToken=$API_TOKEN" "$NODE_QEMU_STATUS_URL" | jq . >> "$node_name/$pure_qemu-status.json"
-done < "node-resources.tmp"
-echo "Nodes QEMU status is saved to each node's directory as qemu-*-status.json"
-
-# node network list
-echo -ne "Fetching node available network\r"
-while IFS= read -r node_name; do
-    NODE_NETWORK_URL="$CLUSTER_URL""nodes/$node_name/network"
-    if [ -f "$node_name/node-network-list.json" ]; then
-        rm "$node_name/node-network-list.json"
-    fi
-    curl -s -H "Authorization: PVEAPIToken=$API_TOKEN" "$NODE_NETWORK_URL" | jq . >> "$node_name/node-network-list.json"
-done < "node-names.tmp"
-echo "Nodes available network list is saved to each node's directory as node-network-list.json"
-
-# node storage status
-echo -ne "Fetching node storage status\r"
-while IFS= read -r node_name; do
-    NODE_STORAGE_STATUS_URL="$CLUSTER_URL""nodes/$node_name/storage"
-    if [ -f "$node_name/node-storage-status.json" ]; then
-        rm "$node_name/node-storage-status.json"
-    fi
-    curl -s -H "Authorization: PVEAPIToken=$API_TOKEN" "$NODE_STORAGE_STATUS_URL" | jq . >> "$node_name/node-storage-status.json"
-done < "node-names.tmp"
-echo "Nodes storage status is saved to each node's directory as node-storage-status.json"
-
-
-# node summary
-echo -ne "Summarizing node information\r"
-while IFS= read -r node_name; do
-  if [[ -d "$node_name" ]]; then
-    if [ -f "$node_name/node-summary.json" ]; then
-        rm "$node_name/node-summary.json"
-    fi
-    total_cpus=0
-    allocated_cpus=0
-    for file in $node_name/[0-9]*.json; do
-        cpus=$(jq -r '.data.cpus' "$file")
-        if [[ -n "$cpus" ]]; then
-            cpus_int=$(echo "$cpus" | tr -dc '[:digit:]')
-            allocated_cpus=$((allocated_cpus + cpus_int))
+for file in *.json; do
+    output_file="$file.tmp"
+    while IFS= read -r line; do
+        if [[ $line == *"}"* ]]; then
+            sed -i '$s/,$//' "$output_file"
+            sed -i 's/": \./": 0\./g' "$output_file"
         fi
-    done
-    total_cpus=$(jq -r '.data.cpuinfo.cpus' $node_name/node-status.json)
-    free_cpus=$((total_cpus - allocated_cpus))
+        echo "$line" >> "$output_file"
+    done < "$file"
+    mv "$output_file" "$file"
+done
 
-    allocated_memory=0
-    for file in $node_name/[0-9]*.json; do
-    max_mem=$(jq -r '.data.maxmem' "$file")
-    if [[ -n "$max_mem" ]]; then
-        mem_int=$(echo "$max_mem" | tr -dc '[:digit:]')
-        allocated_memory=$((allocated_memory + mem_int))
-    fi
-    done
-    total_memory=$(jq -r '.data.memory.total' $node_name/node-status.json)
-
-    # Convert memory values to gigabytes
-    convert_to_gb() {
-    value="$1"
-    echo "$(echo "$value / (1024 * 1024 * 1024)" | bc -l | awk '{printf("%.2f", $1)}')"
-    }
-
-    total_memory_gb=$(convert_to_gb "$total_memory")
-    allocated_memory_gb=$(convert_to_gb "$allocated_memory")
-    free_memory_gb=$(convert_to_gb "$(echo "$total_memory - $allocated_memory" | bc -l)")
-
-    # Prepare the data dictionary
-    data="{
-    \"cpu\": {
-        \"total\": ${total_cpus},
-        \"allocated\": ${allocated_cpus},
-        \"free\": ${free_cpus}
-    },
-    \"memory\": {
-        \"total\": ${total_memory_gb},
-        \"allocated\": ${allocated_memory_gb},
-        \"free\": ${free_memory_gb}
-    }
-    }"
-
-    # Convert data to JSON string
-    echo "$data" | jq '.' > $node_name/node-summary.json
-  fi
-done < "node-names.tmp"
-echo "Nodes information summary is saved to each node's directory as node-summary.json"
+echo "Data processing is done!"
 
 # cleanup
-rm node-names.tmp node-resources.tmp cluster-resources.json
+rm cluster-resources.json
+find . -type f -name '*tmp*' -exec rm {} +
